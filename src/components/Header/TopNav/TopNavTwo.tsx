7084896d6f7f4a60ca2c93540ee9acf0e2b2d3bf
Alexander Oviedo <alexanderoviedo@Alexanders-MacBook-Air.local>
2025-05-10 08:52:30 -0400

    Add missing MenuOne component

--- src/components/Header/TopNav/TopNavTwo.tsx
+++ src/components/Header/TopNav/TopNavTwo.tsx
@@ -11,24 +11,22 @@ const TopNavTwo = () => {
                     <div className="select-block relative">
                         <select className="border-none outline-none bg-dark-blue text-white p-2 caption2">
                             <option value="English">English</option>
-                            <option value="France">France</option>
-                            <option value="Espana">Espana</option>
+                            <option value="Español">Español</option>
                         </select>
                         <Icon.CaretDown weight="bold" className="text-xs text-white icon -right-2" />
                     </div>
                     <div className="right-block flex items-center">
                         <div className="location flex items-center max-lg:hidden">
-                            <i className="ph-light ph-map-pin text-white fs-20"></i>
                             <Icon.MapPin className="text-white text-xl" />
-                            <span className="ml-2 caption1 text-white">160 Broadway 15th floor, New York</span>
+                            <span className="ml-2 caption1 text-white">West Palm Beach, FL</span>
                         </div>
                         <div className="mail lg:ml-7 flex items-center">
                             <Icon.Envelope className="text-white text-xl" />
-                            <span className="ml-2 caption1 text-white">hi.avitex@gmail.com</span>
+                            <span className="ml-2 caption1 text-white">info@fascinantedigital.com</span>
                         </div>
                     </div>
                 </div>
-            </div >
+            </div>
         </>
     )
 }
