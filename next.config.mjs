b4bfde7c595d8d83cee4bec29092344d15248930
Alexander Oviedo <alexanderoviedo@Alexanders-MacBook-Air.local>
2025-05-15 11:26:05 -0400

    Update rewrite to Fascinante Digital booking

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,22 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/",
-        destination: "https://cal.com/fascinante-digital/15min",
+        source: "/:path*",
+        destination: "https://cal.com/fascinante-digital/15min/:path*",
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "X-Forwarded-Host", value: "booking.fascinantedigital.com" },
+          { key: "X-Forwarded-Proto", value: "https" },
+        ],
       },
     ];
   },
